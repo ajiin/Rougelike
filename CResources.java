e574042a6bf80d7c0c621b29a3a3aa7baa359a3b
ajiin <adrianlikesturtles@gmail.com>
2014-06-06 13:11:49 -0400

    Update CResources.java

--- CResources.java
+++ CResources.java
@@ -1,5 +1,23 @@
 class CResources extends CBase{
-  private int maxHP, HP, maxMana, mana;
+  private int maxHP, HP, maxMana, mana, nakedDefense, nakedAttack;
+  public int currentDefense, currentAttack;;
+  
+  public void equipA(Armor i){
+    currentDefense+=i.armorDefense;
+  }
+  
+  public void equipW(Weapon i){
+    currentAttack+=i.damage;
+  }
+  
+  public void unequipA(Armor i){
+    currentDefense-=i.armorDefense;
+  }
+  
+  public void unequipW(Weapon i){
+    currentAttack-=i.damage;
+  }
+  
   public CResources(Entity o){
     super(o);
   }
